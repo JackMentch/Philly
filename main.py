43845377059aeaea788f0438b6a7cfbcb9cbbe9e
jackmentch <j.mentch@icloud.com>
2021-11-02 21:06:18 -0400

    Added Requirements.txt

--- main.py
+++ main.py
@@ -5,35 +5,26 @@ from dataframe_functions import *
 
 link = "https://questionnaire-148920.appspot.com/swe/data.html"
 
+# generates a dataframe from the dataset link
+# fill_in_missing_data and check_for_data_disparity are left False because they significantly slow down system preformance
 df = link_to_dataframe(link=link, fill_in_missing_data=False, check_for_data_disparity = False)
 
-# print(get_stats_from_name("Harper, Bryce", 2016))
+# look at the stats for a given player during 2016
+print(f"Bryce Harper 2016 stats: {get_stats_from_name('Harper, Bryce', 2016)}")
+print(f"Jean Segura 2016 stats: {get_stats_from_name('Segura, Jean', 2016)}")
 
+# sorting out dataset so highest earning players are at the top
 df = sort_by_salary(df=df)
 
-df = nth_largest(df=df, n=15)
+# shortens the dataframe to only the to 125 highest earners
+df = nth_largest(df=df, n=125)
 
-# print(average_salary(df))
+qual_offer = average_salary(df)
 
-# salary_histogram(df=df)
+print(f"\nQualifying Offer: ${qual_offer}")
 
+salary_histogram(df=df)
 
-# salary_war_plot(df=df)
+# salary_war_plot(df)
 
-salary_position_plot(df)
-
-# things to consider
-
-# think about how someone should use this tool. should it be a library? an app?
-
-# who will be using the tool? a developer? a data analyst?
-
-# how do you want to organize the code given all the visualizations/queries you want to add? might be worth looking up
-# some examples of how to structure python projects?
-
-# does the user want to run the same visualizations/queries every time or do they want to be able to pick which queries
-# to run
-
-# are the interesting analyses you want to do using data from outside this webpage?
-
-# (qualOfferProj) jackmentch@Jacks-MacBook-Pro Phillies % conda env export > environment.yml
+# salary_position_plot(df)
